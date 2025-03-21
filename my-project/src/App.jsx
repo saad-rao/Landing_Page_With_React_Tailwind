879e6fd23b8276d494b4ee13ddfce2245189096d
Saad_Naseem <raosad786@gmail.com>
2025-03-21 10:08:24 +0500

    Add Routing

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,3 +1,7 @@
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { useEffect } from 'react';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import './index.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
@@ -10,16 +14,35 @@ import Footer from './components/Footer'
 
 function App() {
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration (in milliseconds)
+      once: true, // Animation only happens once on scroll
+      offset: 100, // Trigger animations 100px before element is in view
+    });
+  }, []);
+
   return (
-    <>
+    <Router>
+
     <Navbar />
     <Hero />
     <WhatWeDo />
     <About/>
     <Services/>
     <Contact/>
+
+
+    <Routes>
+     {/* <Route path="/home" element={} /> */}
+   <Route path='/about' element={ <About/>} />
+   <Route path='/services' element={ <Services/>} />
+    <Route path='/contact' element={<Contact/>} />
+    
+    </Routes>
     <Footer/>
-    </>
+
+    </Router>
   )
 }
 
